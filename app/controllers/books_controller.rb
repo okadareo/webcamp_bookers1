a8a7e848985336ca0c66f669b6ae2fb4455b3027
okadareo <okadareo0614@gmail.com>
2020-12-10 14:32:00 +0000

    [ADD] topページのルートディレクトリ設定追加・books追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,54 @@
 class BooksController < ApplicationController
+  def create
+    book = Book.new(book_params)
+    book.save
+    
+    respond_to do |format|
+      if @book.save
+        format.html { redirect_to  books_path, notice: 'Book was successfully created.' }
+        format.json { render :show, status: :created, location: @book }
+      else
+        format.html { render :new }
+        format.json { render json: @book.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+  
   def index
+    @books = Book.all
+    
+    if params[:id].present?
+      set_user
+    else
+      @book = Book.new
+    end
   end
 
   def show
-  end
-
-  def new
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+  
+  def update
+    respond_to do |format|
+      if @book.update(user_params)
+        format.html { redirect_to request.referer, notice: 'Book was successfully created.' }
+        format.json { render :show, status: :ok, location: @book }
+      else
+        format.html { render :edit }
+        format.json { render json: @book.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+  
+  def destroy
+  end
+  
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,5 @@
 Rails.application.routes.draw do
-  get 'books/index'
-
-  get 'books/show'
-
-  get 'books/new'
-
-  get 'books/edit'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'texts#index'
+  resources :books 
+    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
