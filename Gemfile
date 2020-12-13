8dd515ad06904523bd14e07586b890b1faccce9b
okadareo <okadareo0614@gmail.com>
2020-12-13 07:54:57 +0000

    [Finish] 3-8

--- Gemfile
+++ Gemfile
@@ -7,7 +7,7 @@ end
 
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
-gem 'rails', '~> 5.0.7', '>= 5.0.7.2'
+gem 'rails', '~> 5.2.4', '>= 5.2.4.4'
 # Use sqlite3 as the database for Active Record
 gem "sqlite3", "~> 1.3.6"
 # Use Puma as the app server
@@ -37,7 +37,10 @@ gem 'jbuilder', '~> 2.5'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
-  gem 'byebug', platform: :mri
+  gem 'capybara', '>= 2.15'
+  gem 'rspec-rails'
+  gem 'factory_bot_rails'
+  gem 'faker'
 end
 
 group :development do
@@ -51,3 +54,5 @@ end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+
+gem 'bootsnap', require: false
--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,26 +1,22 @@
 class BooksController < ApplicationController
-  def create
-    book = Book.new(book_params)
-    book.save
-    
-    respond_to do |format|
-      if @book.save
-        format.html { redirect_to  books_path, notice: 'Book was successfully created.' }
-        format.json { render :show, status: :created, location: @book }
-      else
-        format.html { render :new }
-        format.json { render json: @book.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-  
+
   def index
     @books = Book.all
-    
-    if params[:id].present?
-      set_user
+    @book = Book.new
+  end
+
+  # def new
+
+  # end
+
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      # flash[:success] = "Book OK"
+      redirect_to book_path(@book), notice: "Book was successfully created."
     else
-      @book = Book.new
+      @books = Book.all
+      render("books/index")
     end
   end
 
@@ -31,22 +27,41 @@ class BooksController < ApplicationController
   def edit
     @book = Book.find(params[:id])
   end
-  
+
   def update
-    respond_to do |format|
-      if @book.update(user_params)
-        format.html { redirect_to request.referer, notice: 'Book was successfully created.' }
-        format.json { render :show, status: :ok, location: @book }
-      else
-        format.html { render :edit }
-        format.json { render json: @book.errors, status: :unprocessable_entity }
-      end
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+
+
+      # redirect_to book_path(book.id)
+       redirect_to book_path(@book.id), notice: "Book was successfully updateted."
+    else
+
+
+     render("books/edit")
+
+    # <やりたいこと>
+    # もしデータがセーブされなかった場合の処理
+    # 1.バリデーションエラーを返す
+    # 2.編集画面を表示
+
+    #<疑問点>
+    #・renderでshow.htmlに返してhtml側で処理を分岐するべきかもしくはerror.htmlを作成して別途処理を描くべきか
+    #・そもそも上記のロジックであっているか
+
+
+    # ここのあとの処理が分かりません
+    # 更新処理後どのように編集画面に行けば良いかまたどのような処理を描くべきか
+
     end
   end
-  
+
   def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
   end
-  
+
   private
   def book_params
     params.require(:book).permit(:title, :body)
